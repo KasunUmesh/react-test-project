b7d8e25373dd1da5fde2ec63666a23b3421b388c
KasunUmesh <kasunumesh38@gmail.com>
2023-12-27 02:15:29 +0530

    button click count up and down function Created

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+class App extends React.Component<any, any> {
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  state = {
+    count: 0,
+    visible: true
+  };
+
+  up = (): void => {
+    
+    this.setState({count: this.state.count + 1});
+
+  }
+
+  down = (): void => {
+    
+    this.setState({count: this.state.count - 1});
+
+  }
+
+  render() {
+    return(
+
+      <div className={"m-5"}>
+        <button className={"inline bg-green-600 px-5"} onClick={this.up}>+</button>
+        <div className={"inline mx-5 font-bold"}>{this.state.count}</div>
+        <button className={"inline bg-red-600 px-5"} onClick={this.down}>-</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+    );     
+  }
+  
 }
 
 export default App
